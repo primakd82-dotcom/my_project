6c71f767a7a51d1ca406508b40bf075e5a13f583
Дмитрий Примак <primak_d82@mail.ru>
2026-03-29 21:44:00 +0900

    add finish two func

--- src/processing.py
+++ src/processing.py
@@ -12,7 +12,7 @@
 ]"""
 
 
-def filter_by_state(my_list, my_state='EXECUTED'):
+def filter_by_state(my_list, my_state='EXECUTED') -> list:
     """ Функция возвращает новый список словарей, содержащий только те словари,
      у которых ключ state соответствует указанному значению. """
     new_list = []
@@ -23,7 +23,8 @@ def filter_by_state(my_list, my_state='EXECUTED'):
 
     return new_list
 
-list = [
+
+my_list = [
     {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
     {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
     {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
@@ -33,10 +34,11 @@ list = [
 print(filter_by_state(list))
 
 
-def sort_by_date(my_list, descending=True):
+def sort_by_date(my_list, descending=True) -> list:
     """ Функция возвращает новый список,
     отсортированный по дате (date) на убывание """
     return sorted(my_list, key=lambda x: x.get('date'), reverse=descending)
+
 
 print(sort_by_date([
     {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
