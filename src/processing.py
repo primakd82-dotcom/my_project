9bcaba02c87af414f8ae0417d2c283da99ccf670
Дмитрий Примак <primak_d82@mail.ru>
2026-03-29 18:16:35 +0900

    add finish func sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -33,6 +33,14 @@ list = [
 print(filter_by_state(list))
 
 
-def sort_by_date(my_list: str, my_date=True):
-    """ Функция возвращает новый список, отсортированный по дате (date) """
-    new_list_date = []
+def sort_by_date(my_list, descending=True):
+    """ Функция возвращает новый список,
+    отсортированный по дате (date) на убывание """
+    return sorted(my_list, key=lambda x: x.get('date'), reverse=descending)
+
+print(sort_by_date([
+    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+]))
