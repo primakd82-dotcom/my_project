3e0d04e7d819f0a1938e5185033d80026b374308
Дмитрий Примак <primak_d82@mail.ru>
2026-03-30 23:43:27 +0900

    add fixes two func

--- src/processing.py
+++ src/processing.py
@@ -12,32 +12,32 @@
 ]"""
 
 
-def filter_by_state(my_list, my_state='EXECUTED') -> list:
+def filter_by_state(data_list, status='EXECUTED') -> list:
     """ Функция возвращает новый список словарей, содержащий только те словари,
      у которых ключ state соответствует указанному значению. """
     new_list = []
 
-    for item in my_list:
-        if item.get('state') == my_state:
+    for item in data_list:
+        if item.get('state') == status:
             new_list.append(item)
 
     return new_list
 
 
-my_list = [
+data_list = [
     {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
     {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
     {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
     {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
 ]
 
-print(filter_by_state(list))
+print(filter_by_state(data_list))
 
 
-def sort_by_date(my_list, descending=True) -> list:
+def sort_by_date(data_list, descending=True) -> list:
     """ Функция возвращает новый список,
     отсортированный по дате (date) на убывание """
-    return sorted(my_list, key=lambda x: x.get('date'), reverse=descending)
+    return sorted(data_list, key=lambda x: x.get('date'), reverse=descending)
 
 
 print(sort_by_date([
