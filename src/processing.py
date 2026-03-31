ecab11ae7fa524f9725a17d5ea71b24e6636a2c6
Дмитрий Примак <primak_d82@mail.ru>
2026-03-31 20:49:13 +0900

    add fixes two func

--- src/processing.py
+++ src/processing.py
@@ -12,32 +12,32 @@
 ]"""
 
 
-def filter_by_state(data_list, status='EXECUTED') -> list:
+def filter_by_state(transactions: list, status='EXECUTED') -> list:
     """ Функция возвращает новый список словарей, содержащий только те словари,
      у которых ключ state соответствует указанному значению. """
     new_list = []
 
-    for item in data_list:
+    for item in transactions:
         if item.get('state') == status:
             new_list.append(item)
 
     return new_list
 
 
-data_list = [
+transactions = [
     {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
     {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
     {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
     {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
 ]
 
-print(filter_by_state(data_list))
+print(filter_by_state(transactions))
 
 
-def sort_by_date(data_list, descending=True) -> list:
+def sort_by_date(transactions: list, descending=True) -> list:
     """ Функция возвращает новый список,
     отсортированный по дате (date) на убывание """
-    return sorted(data_list, key=lambda x: x.get('date'), reverse=descending)
+    return sorted(transactions, key=lambda x: x.get('date'), reverse=descending)
 
 
 print(sort_by_date([
