a10d4931addf323750cc98780a0b96d472db9d3f
Дмитрий Примак <primak_d82@mail.ru>
2026-05-31 14:50:16 +0900

    finishing modul generators

--- src/generators.py
+++ src/generators.py
@@ -3,3 +3,19 @@ def filter_by_currency(transactions, currency):
     filtered_transactions = filter(lambda x: x["operationAmount"]["currency"]["code"] == currency, transactions)
     return filtered_transactions
 
+
+def transaction_descriptions(transactions):
+    """ Генератор, который принимает список словарей с транзакциями и возвращает
+        описание каждой операции по очереди. """
+    for transaction in transactions:
+        yield transaction["description"]
+
+
+def card_number_generator(start, stop):
+    """ Генератор, который выдает номера банковских карт в формате XXXX XXXX XXXX XXXX. """
+    for number in range(start, stop+1):
+        numbers = str(number).zfill(16)
+        format_num = f"{numbers[:4]} {numbers[4:8]} {numbers[8:12]} {numbers[12:]}"
+        yield format_num
+        if numbers == "9999999999999999":
+            break
